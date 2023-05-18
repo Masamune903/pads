076587ab6b43790b7eeafd162e0f2a83ebcf4b42
Masamune903 <masamune_903@outlook.jp>
2023-05-18 18:39:55 +0900

    change

--- sql_query/create_table.sql
+++ sql_query/create_table.sql
@@ -36,7 +36,24 @@ CREATE TABLE warehouse (
 );
 
 CREATE TABLE delivery_member (
+	code INT PRIMARY KEY,
+	name VARCHAR(50),
+	state VARCHAR(20)
+);
 
+CREATE TABLE route_plan (
+	product_code INT PRIMARY KEY,
+	from_location_name VARCHAR(50),
+	to_location_name VARCHAR(50)
+);
+
+CREATE TABLE delivery (
+	product_code INT PRIMARY KEY,
+	delivery_member_code INT PRIMARY KEY,
+	start_time DATETIME,
+	end_time DATETIME,
+	from_location_name VARCHAR(50),
+	end_location_name VARCHAR(50)
 );
 
 CREATE TABLE user (
@@ -45,10 +62,55 @@ CREATE TABLE user (
 	money INT
 );
 
+CREATE TABLE location_register (
+	id INT PRIMARY KEY,
+	name VARCHAR(50),
+	money INT
+);
 
+/* データの初期化 */
 
-LOAD DATA LOCAL INFILE "initdata/user.csv"
+LOAD DATA LOCAL INFILE "initdata/model.csv"
 	into table user
 	fields terminated by ','
 	lines terminated by '\r\n';
 
+LOAD DATA LOCAL INFILE "initdata/manufacturer.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
+
+LOAD DATA LOCAL INFILE "initdata/product.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
+
+LOAD DATA LOCAL INFILE "initdata/location.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
+
+LOAD DATA LOCAL INFILE "initdata/receipt_location.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
+
+LOAD DATA LOCAL INFILE "initdata/trsp_hub.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
+
+LOAD DATA LOCAL INFILE "initdata/warehouse.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
+
+LOAD DATA LOCAL INFILE "initdata/delivery_member.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
+	
+LOAD DATA LOCAL INFILE "initdata/user.csv"
+	into table user
+	fields terminated by ','
+	lines terminated by '\r\n';
