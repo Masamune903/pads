dcf3cfae2fdfb56740c567a6dbac03f47bd0eb30
Masamune903 <masamune_903@outlook.jp>
2023-05-31 10:08:28 +0900

    update

--- mysql-util/init_database/query.sql
+++ mysql-util/init_database/query.sql
@@ -1,10 +1,8 @@
-/* $ "bin/mysql" -u root --local-infile=1 */
-
+/* initdata: C:/Program Files/MySQL/MySQL 8.0.33/initdata */
+/* $ cd "C:/Program Files/MySQL/MySQL 8.0.33"; bin/mysql -u root --local-infile=1; */
 DROP DATABASE pads;
 CREATE DATABASE pads;
-
 USE pads;
-
 CREATE TABLE model (
 	code VARCHAR(20) PRIMARY KEY,
 	name VARCHAR(50),
@@ -12,11 +10,7 @@ CREATE TABLE model (
 	price INT,
 	manufacturer VARCHAR(50)
 );
-
-CREATE TABLE manufacturer (
-	name VARCHAR(50) PRIMARY KEY
-);
-
+CREATE TABLE manufacturer (name VARCHAR(50) PRIMARY KEY);
 CREATE TABLE product (
 	code VARCHAR(20),
 	model VARCHAR(20),
@@ -27,114 +21,107 @@ CREATE TABLE product (
 	receipt_location VARCHAR(50),
 	PRIMARY KEY (code, model)
 );
-
-CREATE TABLE model_category (
-	name VARCHAR(20)
-);
-
+CREATE TABLE model_category (name VARCHAR(20));
 CREATE TABLE location (
 	name VARCHAR(50) PRIMARY KEY,
-	address VARCHAR(50)
+	address VARCHAR(50),
+	longitude DOUBLE,
+	latitude DOUBLE
 );
-
-CREATE TABLE receipt_location (
-	name VARCHAR(50) PRIMARY KEY
-);
-
-CREATE TABLE trsp_hub (
-	name VARCHAR(50) PRIMARY KEY
-);
-
-CREATE TABLE warehouse (
-	name VARCHAR(50) PRIMARY KEY
-);
-
+CREATE TABLE receipt_location (name VARCHAR(50) PRIMARY KEY);
+CREATE TABLE trsp_hub (name VARCHAR(50) PRIMARY KEY);
+CREATE TABLE warehouse (name VARCHAR(50) PRIMARY KEY);
 CREATE TABLE delivery_member (
 	code VARCHAR(20) PRIMARY KEY,
 	name VARCHAR(50),
 	state VARCHAR(20),
-	belongs_to VARCHAR(50) /* 所属する運送拠点 */
+	belongs_to VARCHAR(50)
+	/* 所属する集積所 */
 );
-
-CREATE TABLE delivery_plan (
-	product VARCHAR(20) PRIMARY KEY,
+CREATE TABLE delivery (
+	product_code VARCHAR(20),
+	product_model VARCHAR(20),
 	from_location VARCHAR(50),
 	to_location VARCHAR(50),
-	delivery_member VARCHAR(20)
-);
-INSERT INTO delivery_plan VALUES ("P001", "L001", "L002", "DM001");
-INSERT INTO delivery_plan VALUES ("P011", "L001", "L003", "DM001");
-
-CREATE TABLE delivery (
-	product VARCHAR(20),
+	order_index INT,
 	delivery_member VARCHAR(20),
 	start_time DATETIME,
 	end_time DATETIME,
-	from_location VARCHAR(50),
-	to_location VARCHAR(50),
-	PRIMARY KEY (product, delivery_member)
+	PRIMARY KEY (product_code, product_model, from_location)
 );
-INSERT INTO delivery VALUES ("P001", "DM001", "2023/05/26 9:55:00", NULL, "L001", "L002");
-
+INSERT INTO delivery
+VALUES (
+		"P001",
+		"P",
+		"L001",
+		"L002",
+		1,
+		"DM001",
+		"2023-05-26 09:55:00",
+		NULL
+	);
+INSERT INTO delivery
+VALUES (
+		"P011",
+		"P",
+		"L001",
+		"L002",
+		1,
+		"DM001",
+		NULL,
+		NULL
+	);
+INSERT INTO delivery
+VALUES (
+		"P021",
+		"P",
+		"名古屋運送拠点",
+		"大阪運送拠点",
+		1,
+		NULL,
+		NULL,
+		NULL
+	);
+INSERT INTO delivery
+VALUES (
+		"P021",
+		"P",
+		"大阪運送拠点",
+		"東京運送拠点",
+		2,
+		NULL,
+		NULL,
+		NULL
+	);
+INSERT INTO delivery
+VALUES (
+		"P031",
+		"P",
+		"大阪運送拠点",
+		"東京運送拠点",
+		1,
+		NULL,
+		NULL,
+		NULL
+	);
 CREATE TABLE user (
 	id INT PRIMARY KEY,
 	name VARCHAR(50),
 	money INT
 );
-
 CREATE TABLE receipt_location_register (
-	user INT PRIMARY KEY,
-	location VARCHAR(50)
+	user INT,
+	location VARCHAR(50),
+	PRIMARY KEY (user, location)
 );
-
 /* データの初期化 */
-
-LOAD DATA LOCAL INFILE "initdata/model.csv"
-	INTO TABLE model
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/manufacturer.csv"
-	INTO TABLE manufacturer
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/product.csv"
-	INTO TABLE product
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/model_category.csv"
-	INTO TABLE model_category
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/location.csv"
-	INTO TABLE location
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/receipt_location.csv"
-	INTO TABLE receipt_location
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/trsp_hub.csv"
-	INTO TABLE trsp_hub
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/warehouse.csv"
-	INTO TABLE warehouse
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-
-LOAD DATA LOCAL INFILE "initdata/delivery_member.csv"
-	INTO TABLE delivery_member
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
-	
-LOAD DATA LOCAL INFILE "initdata/user.csv"
-	INTO TABLE user
-	FIELDS TERMINATED BY ','
-	LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/model.csv" INTO TABLE model FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/manufacturer.csv" INTO TABLE manufacturer FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/product.csv" INTO TABLE product FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/model_category.csv" INTO TABLE model_category FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/location.csv" INTO TABLE location FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/receipt_location.csv" INTO TABLE receipt_location FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/trsp_hub.csv" INTO TABLE trsp_hub FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/warehouse.csv" INTO TABLE warehouse FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/delivery_member.csv" INTO TABLE delivery_member FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+LOAD DATA LOCAL INFILE "initdata/user.csv" INTO TABLE user FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
